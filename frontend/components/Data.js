e568cec47f95b9f878774ee3adc2400699c3a30c
Michael Boro <michaelboro1122@gmail.com>
2020-03-15 17:17:49 -0400

    proper sorting

---
+++ frontend/components/Data.js
@@ -0,0 +1,11 @@
+import {useContext} from 'react'
+import {ScrapeContext} from './ScrapeContext';
+
+export default function Data() {
+    const scrapeData = useContext(ScrapeContext);
+    console.log(scrapeData);
+    return <div>
+        <h2>Data: </h2>
+        <h3>{scrapeData.hey}</h3>
+    </div>
+}
